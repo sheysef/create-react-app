0cfd702254749b36e4856dae6773d1991c3285eb
sheysef <136742236+sheysef@users.noreply.github.com>
2023-08-30 12:29:16 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,19 +5,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+Shivalee Vichhi - Senior Product Designer
+            </p>
+     </header>
+  <p>
+    Hi, thanks for stopping by. This portfolio dives into my most recent work and a few personal design projects.
+      </p>
     </div>
   );
 }
